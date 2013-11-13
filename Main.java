9903d033343a0848f46adaa607f8602f9d653de9
baugh70 <zakkyzakkba@gmail.com>
2013-11-13 14:50:57 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,12 @@
 public class Main {
+  public static void main(String[] args)[
+    
+    System.out.println("Enter a command");
+    Scanner cmd = new Scanner(System.in);
+    
+    if (cmd.equalsIgnoreCase("newemployee")){
+      
+    }
+  ]
 
 }
