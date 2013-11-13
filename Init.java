4413d27d77bce12c62f1b98b6caaa8146b1900a6
baugh70 <zakkyzakkba@gmail.com>
2013-11-13 14:58:20 -0500

    Update Init.java

--- Init.java
+++ Init.java
@@ -1,3 +1,15 @@
 public class Init{
+  File employeeFile = new File("employee.txt")
+  
+  public static void initialize(){
+   if(employeeFile.exists()){
+     System.out.println("Employee file exists!");
+   }else[
+     try{
+       
+       ]catch(Exception e){
+         
+       }
+  }
 
 }
